ab487093f9e80eaf589f28a4fedaeae430ba6635
1074428671@qq.com <1074428671@qq.com>
2020-05-26 12:09:49 +0800

    表单上传文件成功

--- GMMS/Areas/User/Controllers/UploadController.cs
+++ GMMS/Areas/User/Controllers/UploadController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,13 +19,13 @@ namespace GMMS.Areas.User.Controllers
         {
             if (Request.Files.Count > 0)
             {
-                HttpPostedFileBase f = Request.Files["file"];//最简单的获取方法
-                f.SaveAs(AppDomain.CurrentDomain.BaseDirectory + "/images/" + f.FileName);//保存图片
+                HttpPostedFileBase file = Request.Files["file"];//最简单的获取方法
+                file.SaveAs(Path.Combine(Server.MapPath("/images"), Path.GetFileName(file.FileName)));//用Server.MapPath获得相对路径的物理路径
 
                 //这下面是返回json给前端 
                 var data1 = new
                 {
-                    src = AppDomain.CurrentDomain.BaseDirectory + "/images/" + f.FileName,//服务器储存路径
+                    src = AppDomain.CurrentDomain.BaseDirectory + "/images/" + file.FileName,//服务器储存路径
                 };
                 var Person = new
                 {
